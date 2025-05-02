b67114792cceb3f8cba81d7dec2e2e4afc289cf0
sjkuhnke <sjkuhnke@uwm.edu>
2025-05-02 17:45:27 -0500

    Resume section done, worked on contact page

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -19,16 +19,31 @@ def portfolio(request):
 
 def contact(request):
     recaptcha_site_key = settings.GOOGLE_RECAPTCHA_SITE_KEY
+    recaptcha_secret_key = settings.GOOGLE_RECAPTCHA_SECRET_KEY
 
     if request.method == 'POST':
-        name = request.POST.get('name')
+        name = request.POST.get('fullname')
         email = request.POST.get('email')
         message = request.POST.get('message')
         recaptcha_response = request.POST.get('g-recaptcha-response')
 
-        if not name or not email or not message:
+        if not name or not email or not message or not recaptcha_response:
             return render(request, 'contact.html', {
                 'error': 'All fields are required.',
+                'recaptcha_site_key': recaptcha_site_key
+            })
+
+        recaptcha_verify_url = 'https://recaptchaenterprise.googleapis.com/v1/projects/shae-kuhnke-1744126552098/assessments?key=API_KEY'
+        recaptcha_data = {
+            'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+            'response': recaptcha_response
+        }
+        recaptcha_result = requests.post(recaptcha_verify_url, data=recaptcha_data)
+        recaptcha_result_json = recaptcha_result.json()
+
+        if not recaptcha_result_json['success']:
+            return render(request, 'contact.html', {
+                'error': 'reCAPTCHA validation failed. Please try again.',
                 'recaptcha_site_key': recaptcha_site_key
             })
 
@@ -43,13 +58,14 @@ def contact(request):
             email_subject,
             email_body,
             settings.DEFAULT_FROM_EMAIL,
-            ['shaekuhnke@gmail.com']
+            ['shaejk29@gmail.com']
         )
 
         try:
+            print('success')
             email_message.send()
             return render(request, 'contact.html', {
-                'success': 'Thank you for your message. We will get back to you shortly.',
+                'success': 'Thank you for reaching out! I will get back to you shortly!',
                 'recaptcha_site_key': recaptcha_site_key
             })
         except Exception as e:
